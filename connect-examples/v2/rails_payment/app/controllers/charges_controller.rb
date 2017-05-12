af8f5494917064d15e6a8e48998031e1d3b8309e
Francisco Rojas <frojas@squareup.com>
2017-05-12 09:50:56 -0700

    Uses Active Merchant Square to charge credit card

--- connect-examples/v2/rails_payment/app/controllers/charges_controller.rb
+++ connect-examples/v2/rails_payment/app/controllers/charges_controller.rb
@@ -5,10 +5,8 @@ PRODUCT_COST = {
 }
 
 class ChargesController < ApplicationController
-  
+
   def charge_card
-    transaction_api = SquareConnect::TransactionApi.new()
-    
     #check if product exists
     if !PRODUCT_COST.has_key? params[:product_id]
       render json: {:status => 400, :errors => [{"detail": "Product unavailable"}]  }
@@ -16,21 +14,12 @@ class ChargesController < ApplicationController
     end
 
     amount = PRODUCT_COST[params[:product_id]]
-    request_body = {
-      :card_nonce => params[:nonce],
-      :amount_money => {
-        :amount => amount,
-        :currency => 'USD'
-      },
-      :idempotency_key => SecureRandom.uuid
-    }
+    card_nonce = params[:nonce]
 
-    # The SDK throws an exception if a Connect endpoint responds with anything besides 200 (success).
-    # This block catches any exceptions that occur from the request.
-    locationApi = SquareConnect::LocationApi.new()
-    locations = locationApi.list_locations(Rails.application.secrets.square_access_token)
     begin
-      resp = transaction_api.charge(Rails.application.secrets.square_access_token, locations.locations[0].id, request_body)
+      gateway = gateway(locations.locations[1].id)
+      resp = gateway.purchase(amount, card_nonce)
+
     rescue SquareConnect::ApiError => e
       puts 'Error encountered while charging card:'
       puts e.message
@@ -38,23 +27,42 @@ class ChargesController < ApplicationController
       return
     end
     puts resp
-    
+
     data = {
-      amount: amount, 
+      amount: amount,
       user: {
-        name: params[:name], 
-        street_address_1: params[:street_address_1], 
-        street_address_2: params[:street_address_2], 
+        name: params[:name],
+        street_address_1: params[:street_address_1],
+        street_address_2: params[:street_address_2],
         state: params[:state],
         zip: params[:zip],
-        city: params[:city]  
+        city: params[:city]
       },
-      card: resp.transaction.tenders[0].card_details.card
+      card: resp.params['transaction']['tenders'].first['card_details']['card']
     }
 
     # send receipt email to user
-    ReceiptMailer.charge_email(params[:email],data).deliver_now if Rails.env == "development"
-    
+    ReceiptMailer.charge_email(params[:email], data).deliver_now if Rails.env == "development"
+
     render json: {:status => 200}
+  end
+
+  private
+
+  def locations
+    # The SDK throws an exception if a Connect endpoint responds with anything besides 200 (success).
+    # This block catches any exceptions that occur from the request.
+    locationApi = SquareConnect::LocationApi.new()
+    locationApi.list_locations(Rails.application.secrets.square_access_token)
+  end
+
+  def gateway(location_id)
+    credentials = {
+      login: Rails.application.secrets.square_application_id,
+      password: Rails.application.secrets.square_access_token,
+      location_id: location_id,
+      test: false
+    }
+    ActiveMerchant::Billing::SquareGateway.new(credentials)
   end
 end
